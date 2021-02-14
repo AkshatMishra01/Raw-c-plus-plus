4a1d0503d718856d02be702616a72f2082cdcb9e
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-02-14 11:16:09 +0530

    \

--- filehandling.cpp
+++ filehandling.cpp
@@ -1,55 +1,31 @@
-#include <iostream> 
-  
-/* fstream header file for ifstream, ofstream,  
-   fstream classes */
-#include <fstream> 
-  
-using namespace std; 
-  
-// Driver Code 
-int main() 
-{ 
-    // Creation of fstream class object 
-    fstream fio; 
-  
-    string line; 
-  
-    // by default openmode = ios::in|ios::out mode 
-    // Automatically overwrites the content of file, To append 
-    // the content, open in ios:app 
-    // fio.open("sample.txt", ios::in|ios::out|ios::app) 
-    // ios::trunc mode delete all conetent before open 
-    fio.open("sample.txt", ios::trunc | ios::out | ios::in); 
-  
-    // Execute a loop If file successfully Opened 
-    while (fio) { 
-  
-        // Read a Line from standard input 
-        getline(cin, line); 
-  
-        // Press 1 to exit 
-        if (line == "1") 
-            break; 
-  
-        // Write line in file 
-        fio << line << endl; 
-    } 
-  
-    // Execute a loop untill EOF (End of File) 
-    // point read pointer at beginning of file 
-    fio.seekg(0, ios::beg); 
-  
-    while (fio) { 
-  
-        // Read a Line from File 
-        getline(fio, line); 
-  
-        // Print line in Console 
-        cout << line << endl; 
-    } 
-  
-    // Close the file 
-    fio.close(); 
-  
-    return 0; 
-} 
+//file handling in c++
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	fstream fio; //object declaration of the fstream class already included in <bits/stdc++.h> 
+	string text; // string class object declaration
+	
+	fio.open("sample.cpp", ios::trunc | ios::out | ios::in); // default way of entering text in a file using the open function
+	
+	while(fio){ // if file is open, the user is allowed to enter the text
+		
+		getline(cin, text);
+		
+		if (text == "-1") // if input by the user is -1, the program terminates
+			break;
+		
+		fio << text<< endl;// to write the line in the file 
+	}
+	
+	fio.seekg(0, ios::beg);// execute a loop till the end of file EOF
+	
+	while (fio) {
+		getline(cin, text);
+		cout<<text<<endl;	
+	}
+	
+	fio.close(); // closing the file 
+	
+	return 0;	
+}
