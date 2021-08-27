6004107742f95fde12249b251aeb6d798cd850a5
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-08-27 12:24:48 +0530

    Update binary serach.cpp

--- binary serach.cpp
+++ binary serach.cpp
@@ -31,3 +31,36 @@ int main()
     cout<<"Element is at index:"<<Binary_search(arr,0,size-1,key);
     return 0;
 }
+
+// SEcond programn with manually entered array and values
+#include<iostream>
+using namespace std;
+int Binary_search(int arr[],int l,int r,int key){
+if (r>=l)
+{
+    int mid = l + (r-l)/2;
+    if(arr[mid] == key){
+        return mid;
+    }
+    if(arr[mid] > key)
+    {
+        return Binary_search(arr,l,mid -1, key);
+    }
+    return Binary_search(arr,l,mid+1,key);
+}
+return -1;
+
+}
+int main(void)
+{
+    int arr[]={11,12,13,14,15,16,17,18,19,20},size=sizeof(arr),key=17;
+    int result = Binary_search(arr,0, size-1,key);
+    if(result != -1)
+    {
+    cout<<"Element is at index:"<<result;
+    }
+    else{
+        cout<<"Element is not present in the array";
+    }
+    return 0;
+}
