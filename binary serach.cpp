b190e445fb7c1d37e46448e10befb51281230883
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-08-29 13:52:53 +0530

    Update binary serach.cpp

--- binary serach.cpp
+++ binary serach.cpp
@@ -1,66 +1,34 @@
 #include<iostream>
 using namespace std;
-Binary_search(int arr,int l,int r,int key){
-if (r>=l)
+int binary_Search(int arr[],int low,int high,int key)
 {
-    int mid = l + r-l/2;
-    if(arr[mid]==key){
-        return mid;
-    }
-    if(arr[mid]>key)
+    if (high >= low)
     {
-        return Binary_search(arr,l,mid -1, key)
+        int mid = low + (high-low)/2;
+        
+        if(arr[mid] == key)
+        {
+            return mid;
+        }
+        if (arr[mid] > key)
+        {
+            return binary_Search(arr,low,mid-1,key);
+        }
+        return binary_Search(arr,mid+1,high,key);
     }
-    return Binary_search(arr,l,mid+1,key);
-}
-return -1;
-
+    return -1;
 }
 int main()
 {
-    int ar1[100],size,key;
-    cout<<"ENTER THE SIZE OF THE ARRAY:";
+    int arr[100];
+    int size;
     cin>>size;
-    cout<<"ENTER THE ARRAY ELEMANTS:";
-    for(int i; i<size; i++)
+    for(int i=0;i<size;i++)
     {
-        cin >> arr[i];
+        cin>>arr[i];
     }
-    cout<<"ENTER THE NUMBER TO BE SEARCHED";
+    int key;
     cin>>key;
-    cout<<"Element is at index:"<<Binary_search(arr,0,size-1,key);
-    return 0;
-}
-
-// SEcond programn with manually entered array and values
-#include<iostream>
-using namespace std;
-int Binary_search(int arr[],int l,int r,int key){
-if (r>=l)
-{
-    int mid = l + (r-l)/2;
-    if(arr[mid] == key){
-        return mid;
-    }
-    if(arr[mid] > key)
-    {
-        return Binary_search(arr,l,mid -1, key);
-    }
-    return Binary_search(arr,l,mid+1,key);
-}
-return -1;
-
-}
-int main(void)
-{
-    int arr[]={11,12,13,14,15,16,17,18,19,20},size=sizeof(arr),key=17;
-    int result = Binary_search(arr,0, size-1,key);
-    if(result != -1)
-    {
-    cout<<"Element is at index:"<<result;
-    }
-    else{
-        cout<<"Element is not present in the array";
-    }
-    return 0;
+    int index = binary_Search(arr,0,size-1,key);
+    cout<<"Element is present at index:"<<index<<endl;
 }
